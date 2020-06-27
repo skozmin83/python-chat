40e697f65250b7d3cd78e8aa25b365b95116418b
Sergey Kozmin <kozmin.sergey@gmail.com>
2020-06-27 14:28:10 -0400

    changed the message

--- Client.py
+++ Client.py
@@ -5,7 +5,7 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 12345        # The port used by the server
 
-print('Connect client to %s:%s' % (HOST, PORT))
+print('Connecting client to %s:%s' % (HOST, PORT))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'This is Sergey, and that\'s my change')
