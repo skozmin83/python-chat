d1f4974d598af034febbe60437bf03f3366e702c
Sergey Kozmin <kozmin.sergey@gmail.com>
2020-06-27 13:52:04 -0400

    changed the message

--- Client.py
+++ Client.py
@@ -8,7 +8,7 @@ PORT = 12345        # The port used by the server
 print('Connect client to %s:%s' % (HOST, PORT))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'This is Sergey')
+    s.sendall(b'This is Sergey, and that\'s my change')
     data = s.recv(1024)
 
 print('Received: ', repr(data))
