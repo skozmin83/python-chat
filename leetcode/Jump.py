311cb80a3d07db6b1737cb27c5f885599e5031a3
vaskovatk <norka.tanya@gmail.com>
2020-06-29 23:22:33 +0600

    leetcode Jump version 4 Time Limit Exceeded

--- leetcode/Jump.py
+++ leetcode/Jump.py
@@ -7,11 +7,13 @@ class Solution:
         if nums[curIndex] ==0:
             return False
         loopDictionary = {}
-        ret = jumpRecursion(nums,curIndex,lastIndex,loopDictionary)
+        ret = jumpRecursion(nums,curIndex,lastIndex,loopDictionary,curIndex)
         return ret
 
-def jumpRecursion(nums: list, curIndex: int, lastIndex:int, loopDictionary: dict):
+def jumpRecursion(nums: list, curIndex: int, lastIndex:int, loopDictionary: dict, beforeCurIndex:int):
    step =0
+   maxValInJump =0
+   savedIndex =0
    nowIndex = curIndex
    if curIndex not in loopDictionary.keys():
        loopDictionary[curIndex] = 1
@@ -19,16 +21,20 @@ def jumpRecursion(nums: list, curIndex: int, lastIndex:int, loopDictionary: dict
            while step<nums[nowIndex]:
                step+=1
                curIndex+=1
+               if maxValInJump<=nums[curIndex]:
+                   savedIndex = curIndex
+                   maxValInJump = nums[curIndex]
                if curIndex == lastIndex:
                    return True
            if nowIndex != lastIndex-1:
-               if nums[nowIndex]==0:
-                   if nums[nowIndex-1]>1:
-                       ret = jumpRecursion(nums,nowIndex+1,lastIndex,loopDictionary)
-                   else:
-                       return False
-               else:
-                   ret = jumpRecursion(nums,nowIndex+1,lastIndex,loopDictionary)
+                if maxValInJump ==0:
+                    if nums[beforeCurIndex]>nums[curIndex]:
+                        nums[savedIndex] = nums[beforeCurIndex]-savedIndex
+                        ret = jumpRecursion(nums,beforeCurIndex,lastIndex,loopDictionary,0)
+                    else:
+                        return False
+                else:
+                    ret = jumpRecursion(nums,savedIndex,lastIndex,loopDictionary,curIndex)
            else:
                ret = False
        else:
@@ -43,14 +49,15 @@ def test(sol, input, expect):
 
 
 sol = Solution()
-# test(sol,[2,3,1,1,4], True)
-# test(sol,[3,2,1,0,4], False)
-# test(sol,[2,0],True)
-# test(sol,[1,2,3],True)
-# test(sol,[1,0,2],False)
-# test(sol,[2,0,0],True)
-# test(sol,[0,2,3],False)
-# test(sol,[3,0,8,2,0,0,1],True)
-# test(sol,[0],True)
-# test(sol,[1,0,1,0], False)
+test(sol,[2,3,1,1,4], True)
+test(sol,[3,2,1,0,4], False)
+test(sol,[2,0],True)
+test(sol,[1,2,3],True)
+test(sol,[1,0,2],False)
+test(sol,[2,0,0],True)
+test(sol,[0,2,3],False)
+test(sol,[3,0,8,2,0,0,1],True)
+test(sol,[0],True)
+test(sol,[1,0,1,0], False)
 test(sol,[5,9,3,2,1,0,2,3,3,1,0,0],True)
+test(sol,[4,2,0,0,1,1,4,4,4,0,4,0], True)
