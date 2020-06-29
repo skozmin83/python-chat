d26ef85d934a2e6fd8a2da86957de6728f83133d
vaskovatk <norka.tanya@gmail.com>
2020-06-29 22:34:33 +0600

    leetcode Jump version 3 don't work yet))

--- leetcode/Jump.py
+++ leetcode/Jump.py
@@ -2,8 +2,11 @@ class Solution:
     def canJump(self, nums: list) -> bool:
         lastIndex = len(nums)-1
         curIndex = 0
+        if len(nums) == 1:
+            return True
+        if nums[curIndex] ==0:
+            return False
         loopDictionary = {}
-        step = 0
         ret = jumpRecursion(nums,curIndex,lastIndex,loopDictionary)
         return ret
 
@@ -18,8 +21,14 @@ def jumpRecursion(nums: list, curIndex: int, lastIndex:int, loopDictionary: dict
                curIndex+=1
                if curIndex == lastIndex:
                    return True
-           if nowIndex != lastIndex-1 and nums[nowIndex] !=0:
-              ret = jumpRecursion(nums,nowIndex+1,lastIndex,loopDictionary)
+           if nowIndex != lastIndex-1:
+               if nums[nowIndex]==0:
+                   if nums[nowIndex-1]>1:
+                       ret = jumpRecursion(nums,nowIndex+1,lastIndex,loopDictionary)
+                   else:
+                       return False
+               else:
+                   ret = jumpRecursion(nums,nowIndex+1,lastIndex,loopDictionary)
            else:
                ret = False
        else:
@@ -34,11 +43,14 @@ def test(sol, input, expect):
 
 
 sol = Solution()
-test(sol,[2,3,1,1,4], True)
-test(sol,[3,2,1,0,4], False)
-test(sol,[2,0],True)
-test(sol,[1,2,3],True)
-test(sol,[1,0,2],False)
-test(sol,[2,0,0],True)
-test(sol,[0,2,3],False)
-test(sol,[3,0,8,2,0,0,1],True)
+# test(sol,[2,3,1,1,4], True)
+# test(sol,[3,2,1,0,4], False)
+# test(sol,[2,0],True)
+# test(sol,[1,2,3],True)
+# test(sol,[1,0,2],False)
+# test(sol,[2,0,0],True)
+# test(sol,[0,2,3],False)
+# test(sol,[3,0,8,2,0,0,1],True)
+# test(sol,[0],True)
+# test(sol,[1,0,1,0], False)
+test(sol,[5,9,3,2,1,0,2,3,3,1,0,0],True)
