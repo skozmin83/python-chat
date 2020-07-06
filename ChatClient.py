b2a49b8b55e7bfe0ce966dc3cc08556f69f946de
vaskovatk <norka.tanya@gmail.com>
2020-07-06 23:50:33 +0600

    Added input name and message

--- ChatClient.py
+++ ChatClient.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
 
-import sys
 import socket
 import Logging
 logger = Logging.getLogger('error')
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 12346        # The port used by the server
-if len(sys.argv)<2:
-    logger.info('Error! Need a this client name argument as arg #1!')
-    exit(14)
-if len(sys.argv) < 3:
-    logger.info('Error! Need an another client name as arg #2')
-    exit(15)
 
-clientName = sys.argv[1]
-toClient = sys.argv[2]
-
-
-logger.info('Connecting client to {}:{} as client {} (talking to {})'.format(HOST, PORT, clientName, toClient))
+name = input('your name is')
+msg = input('your message is')
+msg_to = input('your message for:')
+logger.info('Connecting client to {}:{} as client {} (talking to {})'.format(HOST, PORT, name, msg_to))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'name:' + bytes(clientName, "UTF-8") + b';')
-    s.sendall(b'msg:Hi;')
-    s.sendall(b'msg-to-client:' + bytes(toClient, "UTF-8") + b':test message;')
+    s.sendall(b'name:' +bytes(name,'UTF-8')+ b';')
+    s.sendall(b'msg:'+bytes(msg, 'UTF-8')+ b';')
+    s.sendall(b'msg-to-client:'+bytes(msg_to,'UTF-8')+ b':' + bytes(msg, 'UTF-8') + b';')
     while True:
         receivedData = s.recv(1024)
         logger.info('Received:', repr(receivedData))
