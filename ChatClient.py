b54833a923eed6bafa4a80a9683d13216b17ac73
vaskovatk <norka.tanya@gmail.com>
2020-07-06 23:07:19 +0600

    Add loggers

--- ChatClient.py
+++ ChatClient.py
@@ -2,19 +2,22 @@
 
 import sys
 import socket
-
+import Logging
+logger = Logging.getLogger('error')
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 12346        # The port used by the server
-
+if len(sys.argv)<2:
+    logger.info('Error! Need a this client name argument as arg #1!')
+    exit(14)
 if len(sys.argv) < 3:
-    print("Error! Need a this client name argument as arg #1!")
-    print("Error! Need a another client name name argument as arg #2!")
+    logger.info('Error! Need an another client name as arg #2')
     exit(15)
 
 clientName = sys.argv[1]
 toClient = sys.argv[2]
 
-print('Connecting client to {}:{} as client {} (talking to {})'.format(HOST, PORT, clientName, toClient))
+
+logger.info('Connecting client to {}:{} as client {} (talking to {})'.format(HOST, PORT, clientName, toClient))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'name:' + bytes(clientName, "UTF-8") + b';')
@@ -22,8 +25,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.sendall(b'msg-to-client:' + bytes(toClient, "UTF-8") + b':test message;')
     while True:
         receivedData = s.recv(1024)
-        print('Received:', repr(receivedData))
+        logger.info('Received:', repr(receivedData))
         if not receivedData:
             break
-
-print('Finish client on %s:%s' % (HOST, PORT))
+logger.info('Finish client on %s:%s' % (HOST, PORT))
