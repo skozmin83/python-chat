3517d081d554b7e84b2a875d9e13762d12d4d954
vaskovatk <norka.tanya@gmail.com>
2020-07-19 00:46:48 +0600

    * first workning solution on hard level. faster than 30,7%

--- leetcode/Jump 2.py
+++ leetcode/Jump 2.py
@@ -11,82 +11,27 @@ class Solution:
                 stepsDict[lenght+i] = countOfSteps
             else:
                 if nums[i] !=0:
-                    maxStep = i+nums[i]
-                    if stepsDict[lenght+maxStep] !=0:
-                        countOfSteps =1+stepsDict[lenght+maxStep]
-                        stepsDict[lenght+i] =countOfSteps
-                    else:
+                    maxIndex = i+nums[i]
+                    minCount =999999999999
+                    if stepsDict[lenght+maxIndex] !=1:
+                        for index in range(maxIndex,i,-1):
+                            if stepsDict[index+lenght]!=0 and stepsDict[index+lenght]<minCount:
+                                minCount= stepsDict[index+lenght]
+                                maxIndex = index
+                            else:
+                                continue
+                    if nums[maxIndex] == 0:
                         stepsDict[lenght+i] =0
+                    else:
+                        if stepsDict[lenght+maxIndex] !=0:
+                            countOfSteps=1+stepsDict[lenght+maxIndex]
+                        else:
+                            countOfSteps =0
+                        stepsDict[lenght+i] =countOfSteps
+
                 else:
                     stepsDict[lenght+i] = 0
-            print(stepsDict)
-        minStep = 999999999999
-        if len(stepsDict)>1 and stepsDict[0] !=1:
-            for j in range (1,nums[0]+1):
-                if stepsDict[j]<minStep and stepsDict[j] !=0:
-                    minStep = stepsDict[j]
-            return minStep+1
-        else:
-            return stepsDict[0]
-
-
-
-
-
-
-
-# class Solution:
-#     def jump(self, nums:list) -> int:
-#         if nums[0] ==0 or len(nums) ==1:
-#             return 0
-#         lastIndex = len(nums)-1
-#         stepsArray =[9999999999999999]
-#         retArray = jumpRecursion(nums,0,lastIndex,0,stepsArray)
-#         return retArray[0]
-# def jumpRecursion(nums:list,curIndex:int,lastIndex:int, countOfSteps:int, stepsArray:list):
-#     countOfSteps+=1
-#     if curIndex+nums[curIndex]<lastIndex:
-#         saved = nums[curIndex]
-#         while saved>0:
-#             curIndex+=1
-#             saved-=1
-#             jumpRecursion(nums,curIndex,lastIndex,countOfSteps,stepsArray)
-#     else:
-#         if countOfSteps<stepsArray[0]:
-#             stepsArray[0] = countOfSteps
-#     return stepsArray
-#
-#
-
-    # def jump(self, nums: list) -> int:
-
-        # if nums[0] == 0 or len(nums) == 1:
-        #     return 0
-        # lastIndex = len(nums)-1
-        # curIndex = 0
-        # indexOfMax = 0
-        # maxNum =0
-        # countOfSteps =0
-        # saved = nums[curIndex]
-        # if curIndex<lastIndex:
-        #     while True:
-        #         if curIndex+saved<lastIndex:
-        #             countOfSteps +=1
-        #             while saved>0:
-        #                 saved-=1
-        #                 curIndex+=1
-        #                 if maxNum<=nums[curIndex]:
-        #                     maxNum = nums[curIndex]
-        #                     indexOfMax =curIndex
-        #             else:
-        #                 saved = maxNum
-        #                 maxNum =0
-        #                 curIndex =indexOfMax
-        #
-        #         else:
-        #             countOfSteps+=1
-        #             return countOfSteps
-        #
+        return stepsDict[0]
 
 
 
@@ -109,3 +54,5 @@ test(sol,[1,2,1,1,1],3)
 test(sol,[10,9,8,7,6,5,4,3,2,1,1,0],2)
 test(sol,[2,3,0,1,4],2)
 test(sol,[5,9,3,2,1,0,2,3,3,1,0,0],3)
+test(sol,[5,4,0,1,3,6,8,0,9,4,9,1,8,7,4,8],3)
+test(sol,[2,3,5,9,0,9,7,2,7,9,1,7,4,6,2,1,0,0,1,4,9,0,6,3],5)
