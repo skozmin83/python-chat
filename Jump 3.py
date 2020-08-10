7ba5be70154b6a7c2dfed41ae4896474b0cea486
vaskovatk <norka.tanya@gmail.com>
2020-08-11 00:47:46 +0600

    *change jump 3 solution

--- Jump 3.py
+++ Jump 3.py
@@ -7,18 +7,40 @@ class Solution:
         for i in range (0, lenght):
             if arr[i] ==0:
                 zeroDict[i] = False
-        nextPosIndex = start
-        nextNegIndex = start
-        while True:
-            if arr[nextPosIndex] == 0:
-                zeroDict[nextPosIndex] = True
-            if arr[nextNegIndex] == 0:
-                zeroDict[nextNegIndex] = True
-            if nextPosIndex + arr[nextPosIndex] <=lastIndex:
-                nextPosIndex = nextPosIndex+arr[nextPosIndex]
-            if nextNegIndex - arr[nextNegIndex]>=0:
-                
-
+        if start+arr[start]> lastIndex:
+            curIndexMax = lastIndex
+        else:
+            curIndexMax = start+arr[start]
+        if start-arr[start]<0:
+            curIndexMin = 0
+        else:
+            curIndexMin = start-arr[start]
+        jumRecursion(arr,zeroDict,jumpedIndexes,lastIndex,start, curIndexMax,curIndexMin)
+        if False in zeroDict.values():
+            return False
+        else:
+            return True
+def jumRecursion (arr: list, zeroDict: dict, jumpedIndexes: list, lastIndex: int, curIndex: int, curIndexMax: int,curIndexMin: int):
+    if curIndex not in jumpedIndexes:
+        jumpedIndexes.append(curIndex)
+        if curIndex in zeroDict.keys():
+            del zeroDict[curIndex]
+        if curIndex < curIndexMax:
+            curIndex = curIndex+1
+        else:
+            if curIndex+arr[curIndex]<=lastIndex:
+                curIndexMax = curIndex+arr[curIndex]
+            else:
+                curIndexMax = lastIndex
+        jumRecursion(arr, zeroDict, jumpedIndexes, lastIndex, curIndex, curIndexMax, curIndexMin)
+        if curIndex > curIndexMin:
+            curIndex =curIndex-1
+        else:
+            if curIndex - arr[curIndex]>=0:
+                curIndexMin = curIndex - arr[curIndex]
+            else:
+                curIndexMin = 0
+        jumRecursion(arr,zeroDict,jumpedIndexes,lastIndex,curIndex,curIndexMax,curIndexMin)
 
 
 def test(sol, inputArr, start, expect):
@@ -26,5 +48,6 @@ def test(sol, inputArr, start, expect):
     print('inputArr = {}, startIndex = {}, expect = {}, output ={}, result = {}'.format(inputArr, start,  expect, ret, ret == expect))
 
 sol = Solution()
-test(sol, [4,2,3,0,3,1,2], 5, True)
-test(sol, [3,0,2,1,2],2, False)
+# test(sol, [4,2,3,0,3,1,2], 5, True)
+# test(sol, [3,0,2,1,2],2, False)
+test(sol,[0,3,0,6,3,3,4],6, True)
